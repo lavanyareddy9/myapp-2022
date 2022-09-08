c6187b61d5ed192f1c8f3996f8875f9a041dc807
ankithbolem <100229084+ankithbolem@users.noreply.github.com>
2022-09-08 22:05:57 +0530

    Create tomcat.groovy

---
+++ var/tomcat.groovy
@@ -0,0 +1,16 @@
+@Library("prasadlibs") _
+pipeline{
+    agent any
+    stages{
+        stage("Maven Build"){
+            steps{
+                sh 'mvn clean package -DskipTests=true'
+            }
+        }
+        stage(" Dev Tomcat Deploy"){
+            steps{
+                tomcatDeploy("172.31.1.213","ec2-user","tomcat-dev")
+            }
+        }
+    }
+}
